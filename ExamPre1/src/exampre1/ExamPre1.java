35487adf57030cd059c7b9227d9f05543f7eca0c
ismlcm <ismlcam@me.com>
2015-09-04 09:59:08 +0200

    Git test.

---
+++ ExamPre1/src/exampre1/ExamPre1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exampre1;
+
+/**
+ *
+ * @author Ismail Cam
+ */
+public class ExamPre1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main( String[] args )
+    {
+        // TODO code application logic here
+        //Test
+    }
+    
+}
